1198f1711585f4feaba1adc6df155d1d11442297
Angus Barnes <angus.barnes@outlook.com>
2016-04-19 08:45:03 +0100

    Added button array. Finding adjacent buttons doesn't currently work.

--- Minesweeper/Form1.Designer.cs
+++ Minesweeper/Form1.Designer.cs
@@ -38,31 +38,25 @@ namespace Minesweeper
         /// </summary>
         private void InitializeComponent()
         {
-            this.button_array = new list<System.Windows.Forms.Button()>;
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            for (int y = 0; y < 10; y++)
-            {
-                for (int x = 0; x < 10; x++)
-                {
-                    this.button1 = new System.Windows.Forms.Button();
-                    this.SuspendLayout();
-
-                    this.button1.Location = new System.Drawing.Point(16 + x * 16, 16 + y * 16);
-                    this.button1.Name = "button_" + x.ToString() + "_" + y.ToString();
-                    this.button1.Size = new System.Drawing.Size(16, 16);
-                    this.button1.TabIndex = 0;
-                    this.button1.Text = "";
-                    this.button1.UseVisualStyleBackColor = true;
-                }
-            }
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(16, 16);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(96, 48);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1210, 787);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonStart);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -71,7 +65,8 @@ namespace Minesweeper
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonNew;
+        private System.Windows.Forms.Button buttonStart;
     }
 }
 
--- Minesweeper/Form1.cs
+++ Minesweeper/Form1.cs
@@ -12,9 +12,81 @@ namespace Minesweeper
 {
     public partial class Form1 : Form
     {
+
+        List<System.Windows.Forms.Button> buttonList;
+        int buttonDimensions;
+        int gridWidth;
+        int gridHeight;
+
         public Form1()
         {
             InitializeComponent();
+            buttonDimensions = 32;
+            gridWidth = 10;
+            gridHeight = 10;
+        }
+
+        private void mineButtonClick(object sender, EventArgs e)
+        {
+            string locationString = "";
+            for (int i = 0; i < buttonList.Count; i++)
+            {
+                if (sender == buttonList[i])
+                {
+                    int indexAbove = i - gridWidth;
+                    int indexBelow = i + gridWidth;
+                    int indexRight = i + 1;
+                    int indexLeft = i - 1;
+
+                    if (indexAbove >= 0)
+                    {
+                        buttonList[indexAbove].Hide();
+                    }
+                    if ((indexLeft >= 0) & ((int)(i / gridWidth) == (int)(indexRight / gridWidth)))
+                    {
+                        buttonList[indexLeft].Hide();
+                    }
+                    if ((indexRight < buttonList.Count) & ((int)(i / gridWidth) == (int)(indexRight / gridWidth)))
+                    {
+                        buttonList[indexRight].Hide();
+                    }
+                    if (indexBelow < buttonList.Count)
+                    {
+                        buttonList[indexBelow].Hide();
+                    }
+                }
+            }
+            Console.WriteLine(sender.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            buttonList = new List<System.Windows.Forms.Button> { };
+            SuspendLayout();
+            //SuspendLayout();
+            for (int y = 0; y < gridWidth; y++)
+            {
+                for (int x = 0; x < gridHeight; x++)
+                {
+                    buttonNew = new System.Windows.Forms.Button();
+                    //SuspendLayout();
+                    int locationX = buttonDimensions + x * buttonDimensions;
+                    int locationY = buttonDimensions + y * buttonDimensions;
+                    buttonNew.Location = new System.Drawing.Point(locationX, locationY);
+                    buttonNew.Name = "button_" + x.ToString() + "_" + y.ToString();
+                    buttonNew.Size = new System.Drawing.Size(buttonDimensions, buttonDimensions);
+                    buttonNew.TabIndex = 0;
+                    buttonNew.Text = "";
+                    buttonNew.UseVisualStyleBackColor = true;
+
+                    buttonNew.Click += new EventHandler(mineButtonClick);
+                    //newButton.Click += new System.EventHandler(newButton);
+
+                    buttonList.Add(buttonNew);
+                    Controls.Add(buttonNew);
+                }
+            }
+            ResumeLayout(false);
         }
     }
 }
