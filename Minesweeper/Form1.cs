515d43300c406b3b173719812cbd4267af71b3f5
Angus Barnes <angus.barnes@outlook.com>
2016-04-20 17:57:09 +0100

    Added getAdjacentCells method to GameGrid. Added getCell method to gameGrid. Small changes. Reworked commentry.

--- Minesweeper/Form1.cs
+++ Minesweeper/Form1.cs
@@ -11,42 +11,53 @@ using System.Windows.Forms;
 namespace Minesweeper
 {
     enum cellState { normal, flagged, unsure, empty, numbered, exploded };
+    /// <summary>
+    /// A coordinate
+    /// </summary>
     public class Coordinate
     {
-        /// <summary>
-        /// A coordinate
-        /// </summary>
         private int xOrdinate;
         private int yOrdinate;
 
+        /// <summary>
+        /// A coordinate
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
         public Coordinate(int x, int y)
         {
-            ///<summary>
-            ///A coordinate
-            ///</summary>
-            ///<param name="x">The x ordinate</param>
-            ///<param name="y">The y ordinate</param>
             xOrdinate = x;
             yOrdinate = y;
         }
-
+        /// <summary>
+        /// Gets the coordinate
+        /// </summary>
+        /// <returns>An integer array, [x, y]</returns>
         public int[] get()
         {
-            ///<summary>Gets the coordinate</summary>
-            ///<returns>
-            ///The coordinates as an integer array, [x, y]
-            ///</returns>
             return new int[] { xOrdinate, yOrdinate };
         }
+        /// <summary>
+        /// Sets the ordinates of the coordinate
+        /// </summary>
+        /// <param name="x">The x ordinate</param>
+        /// <param name="y">The y ordinate</param>
         public void set(int x, int y)
         {
-            ///<summary>
-            ///Sets the coordinates
-            ///</summary>
-            ///<param name="x">The x coordinate</param>
-            ///<param name="y">The y coordinate</param>
             xOrdinate = x;
             yOrdinate = y;
+        }
+        /// <summary>
+        /// Adds two coordinates together, dimension by dimension
+        /// </summary>
+        /// <param name="c1">The first coordinate</param>
+        /// <param name="c2">The second coordinate</param>
+        /// <returns>A single coordinate - the result of adding the two together</returns>
+        public static Coordinate operator +(Coordinate c1, Coordinate c2)
+        {
+            int newX = c1.get()[0] + c2.get()[1];
+            int newY = c1.get()[0] + c2.get()[1];
+            return new Coordinate(newX, newY);
         }
     }
     public class GameGrid
@@ -54,20 +65,59 @@ namespace Minesweeper
         /// <summary>
         /// A grid of cells. Each cell keeps track of its own state.
         /// </summary>
-        List<GridCell> cellList;
+        private GridCell[,] cellList;
         public int buttonDimensions;
         public int gridWidth;
         public int gridHeight;
-        public GameGrid(int gridWidth, int gridHeight)
+        public GameGrid(int width, int height)
         {
-            this.gridWidth = gridWidth;
-            this.gridHeight = gridHeight;
-
+            gridWidth = width;
+            gridHeight = height;
+            cellList = new GridCell[gridWidth, gridHeight];
         }
 
-        public GridCell[] getAdjacentCells(Coordinate)
+        public GridCell getCell(Coordinate cellCoordinate)
         {
-            return new GridCell[] { };
+            return cellList[cellCoordinate.get()[0], cellCoordinate.get()[1]];
+        }
+
+        public List<GridCell> getAdjacentCells(Coordinate centreCellCoordinate)
+        {
+            ///<summary>
+            ///Gets all cells adjacent to this one
+            ///</summary>
+            ///<returns>
+            ///Adjacent cells in a list - moves left, right, down, left, right, etc
+            /// </returns>
+            //Create empty list
+            List<GridCell> adjacentCellsList = new List<GridCell> { };
+            //Move left, right, down, left, right, etc. to populate adjacent cells list
+            for (int y = -1; y < 2; y++)
+            {
+                for (int x = -1; x < 2; x++)
+                {
+                    Coordinate offsetCoordinate = new Coordinate(x, y);
+                    //If it's the centre cell, ignore it
+                    if ((x == 0) & (y == 0))
+                    {
+                        continue;
+                    }
+                    //If it's too far left or right, ignore it
+                    if ((x < 0) | (x >= gridWidth))
+                    {
+                        continue;
+                    }
+                    //If it's too far up or down, ignore it
+                    if ((y < 0) | (y >= gridHeight))
+                    {
+                        continue;
+                    }
+                    //It's passed all tests - add it to the adjacent cells list
+                    Coordinate newAdjacentCellCoordinate = offsetCoordinate + centreCellCoordinate;
+                    adjacentCellsList.Add(getCell(newAdjacentCellCoordinate));
+                }
+            }
+            return adjacentCellsList;
         }
 
         private void populateGrid()
@@ -88,26 +138,25 @@ namespace Minesweeper
         Button cellButton;
         bool mined;
         cellState state;
-        Coordinate coordinate;
+        Coordinate position;
 
         public GridCell(GameGrid parentGrid, Coordinate assignedIndex)
         {
             parent = parentGrid;
             state = cellState.normal;
             mined = false;
-            coordinate = assignedIndex;
+            position = assignedIndex;
         }
         private Button CreateButton()
         {
             Button newButton = new Button;
 
-            int locationX = parent.buttonDimensions * coordinate.get()[0];
-            int locationY = parent.buttonDimensions * coordinate.get()[1];
+            int locationX = parent.buttonDimensions * position.get()[0];
+            int locationY = parent.buttonDimensions * position.get()[1];
             newButton.Location = new Point(locationX, locationY);
 
             newButton.Name = "button_" + locationX.ToString() + "_" + locationY.ToString();
             newButton.Size = new Size(locationX, locationY);
-            newButton.TabIndex = index;
             newButton.Text = "-";
             newButton.UseVisualStyleBackColor = true;
 
@@ -124,7 +173,7 @@ namespace Minesweeper
             }
             else
             {
-                GridCell[] adjacentCells = parent.GetAdjacentCells(index);
+                GridCell[] adjacentCells = parent.getAdjacentCells(position);
             }
         }
         private void ToggleFlagState()
